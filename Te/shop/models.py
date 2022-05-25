66700f8bb8b399aa1099dfdf5c39e6b25496d38d
DePoeale <eracha8@gmail.com>
2022-05-25 12:42:06 +0300

    fix models  add image

--- Te/shop/models.py
+++ Te/shop/models.py
@@ -7,11 +7,12 @@ class Product(models.Model):
     name_product = models.CharField(max_length=150,  verbose_name="Название товара")
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL", null=True)
     description_product = models.TextField(verbose_name="Описание товара")
+    image = models.ImageField(upload_to="photos_product/%Y/%m/%d/", verbose_name="Фото", null=True)
     price_product = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
     is_published = models.BooleanField(default=True, verbose_name="Публикация")
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Публикация")
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категория")
     
     def __str__(self):
         return self.name_product
@@ -23,6 +24,16 @@ class Product(models.Model):
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
         ordering = ['time_create', 'name_product']
+        
+class ProductPhoto(models.Model):
+
+    product = models.ForeignKey('Product', on_delete=models.DO_NOTHING, related_name='images')
+    image = models.ImageField(upload_to='products_product/%Y/%m/%d', blank=True, default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    update = models.DateTimeField(auto_now_add=False, auto_now=True)
+    
+    def __str__(self):
+        return self.product.name_product
               
 class Category(models.Model):
 
@@ -42,12 +53,3 @@ class Category(models.Model):
         verbose_name_plural = "Виды чая, посуда"
         ordering = ['id']
     
-class ProductPhoto(models.Model):
-
-    product = models.ForeignKey('Product', on_delete=models.DO_NOTHING, related_name='images')
-    image = models.ImageField(upload_to='products_product/%Y/%m/%d', blank=True, default=True)
-    created = models.DateTimeField(auto_now_add=True, auto_now=False)
-    update = models.DateTimeField(auto_now_add=False, auto_now=True)
-    
-    def __str__(self):
-        return self.product.name_product
